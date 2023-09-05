f31dd9837ed4c1ee8cac2223463c1957b6a3fbd3
Mr M <Bmatthews@fortsmithschools.org>
2023-09-05 09:59:45 -0500

    as

---
+++ SetupNewMacLabPC.sh
@@ -0,0 +1,10 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update && brew upgrade
+brew install --cask visual-studio-code
+brew install python3
+brew install putty
+brew install --cask google-chrome
+brew install --cask powershell
+brew install --cask utm
+brew install --cask alt-tab
