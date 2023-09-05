20336e1fac36f963ea208ea9bfc3f5e5a9900f1e
Mr M <Bmatthews@fortsmithschools.org>
2023-09-05 10:43:40 -0500

    asdfg

---
+++ new.ps1
@@ -0,0 +1,4 @@
+#elevate permissions
+Start-Process powershell.exe -Verb runAs
+#install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
